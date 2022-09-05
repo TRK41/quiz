437a54f4d2c3abbbcb80f13389ad7ef947ceeaf2
TRK41 <Troyk@bell.net>
2022-09-05 16:41:08 -0400

    adding code

---
+++ script.js
@@ -0,0 +1,10 @@
+const questions = [
+  
+    { text: 'what is the answer',
+        choices: ['choice1','correct','choice3','choice4']   },    
+    { text: 'what is the answer',
+        choices: ['choice1','choice2','choice3','correct']},
+    { text: 'what is the answer',
+        choices: ['choice1','choice2','correct','choice4']}    
+
+]
